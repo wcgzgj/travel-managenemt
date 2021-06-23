895e331e84407ff6595139ca8074843c8eb17143
wcgzgj <zhangjia_good@126.com>
2021-06-23 16:34:36 +0800

    项目创建

---
+++ src/test/java/travelManagement/TravelManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package travelManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
